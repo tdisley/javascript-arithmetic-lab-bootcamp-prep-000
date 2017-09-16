66fdcaab99cc7a0d0b6a0214603d5fadbc0d6f7c
t dis <thomdisley@gmail.com>
2017-09-16 09:21:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ function dec(n) {
   return n;
 }
 
-function makeInt(n, 10) {
-  return parseInt(n);
+function makeInt(n) {
+  return parseInt(n, 10);
 }
 
-function preserveDecimal(n, 10) {
-  return parseFloat(n);
+function preserveDecimal(n) {
+  return parseFloat(n, 10);
 }
